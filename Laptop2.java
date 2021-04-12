4e79f3fcc6541e93fecb6a765f71abd35c7e1363
HairulHaqimy <81030381+HairulHaqimy@users.noreply.github.com>
2021-04-12 21:59:21 +0800

    Add files via upload

---
+++ Laptop2.java
@@ -0,0 +1,43 @@
+
+public class Laptop2 {
+
+
+String colour, brand;
+double storage, weight;
+
+
+			//setter
+			public void setColour(String colour) {
+				this.colour = colour;
+			}
+			
+			public void setBrand(String brand) {
+				this.brand = brand;
+			}
+			
+			public void setStorage(double storage) {
+				this.storage = storage;
+			}
+									
+			public void setWeight(double weight) {
+				this.weight = weight;
+			}
+			
+			
+			//getter
+			public String getColour() {
+				return this.colour;
+			}
+			
+			public String getBrand() {
+				return this.brand;
+			}
+			
+			public double getStorage() {
+				return this.storage;
+			}
+			
+			public double getWeight() {
+				return this.weight;
+			}
+	}
